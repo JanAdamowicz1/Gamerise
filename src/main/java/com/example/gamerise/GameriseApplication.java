ae58989bac5388e8d7d2ae626299c748bf44ec83
JanAdamowicz1 <adamowiczj01@gmail.com>
2024-03-17 18:22:28 +0100

    Initial commit

---
+++ src/main/java/com/example/gamerise/GameriseApplication.java
@@ -0,0 +1,13 @@
+package com.example.gamerise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameriseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameriseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gamerise/GameriseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gamerise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameriseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
