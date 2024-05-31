e2e5377005d50ed87da4c0be6aa329b6b15cfa95
JanAdamowicz1 <adamowiczj01@gmail.com>
2024-05-31 22:27:04 +0200

    added rabbitMQ and mail sender

--- src/main/java/com/example/gamerise/api/controller/UserObservedAccountController.java
+++ src/main/java/com/example/gamerise/api/controller/UserObservedAccountController.java
@@ -4,6 +4,7 @@ import com.example.gamerise.api.dto.UserDto;
 import com.example.gamerise.api.model.Game;
 import com.example.gamerise.api.model.User;
 import com.example.gamerise.api.model.UserObservedAccount;
+import com.example.gamerise.publisher.RabbitMQProducer;
 import com.example.gamerise.service.UserService;
 import com.example.gamerise.service.UserObservedAccountService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,18 +18,26 @@ import java.util.Optional;
 public class UserObservedAccountController {
     private final UserService userService;
     private final UserObservedAccountService userObservedAccountService;
+    private final RabbitMQProducer rabbitMQProducer;
 
     @Autowired
-    public UserObservedAccountController(UserService userService, UserObservedAccountService userObservedAccountService) {
+    public UserObservedAccountController(UserService userService, UserObservedAccountService userObservedAccountService, RabbitMQProducer rabbitMQProducer) {
         this.userService = userService;
         this.userObservedAccountService = userObservedAccountService;
+        this.rabbitMQProducer = rabbitMQProducer;
     }
 
     @PostMapping("/follow/{observingUserId}/{observedUserId}")
     public UserObservedAccount followUser(@PathVariable int observingUserId, @PathVariable int observedUserId) {
         User observingUser = userService.getUserById(observingUserId).orElseThrow(() -> new RuntimeException("User not found"));
         User observedUser = userService.getUserById(observedUserId).orElseThrow(() -> new RuntimeException("User not found"));
-        return userObservedAccountService.followUser(observingUser, observedUser);
+
+        UserObservedAccount userObservedAccount = userObservedAccountService.followUser(observingUser, observedUser);
+
+        String message = "User " + observingUser.getUsername() + " is now following you!";
+        rabbitMQProducer.sendMessage(observedUser.getEmail(), message);
+
+        return userObservedAccount;
     }
 
     @GetMapping("/observing/{userId}")
